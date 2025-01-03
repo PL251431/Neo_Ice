7e052c1b72631187fb4d1b7324242e1cce18dabb
PL <pedrolucasdesousaoliveira@aluno.uespi.br>
2025-01-02 21:01:28 -0300

    feat: adiconando o arquivo do provider dos produtos

---
+++ lib/providers/produto_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:neo_ice/database/app_database.dart';
+
+final produtoProvider = StateNotifierProvider<ProdutoNotifier, List<Produto>>(
+  (ref) => ProdutoNotifier(),
+);
+
+class ProdutoNotifier extends StateNotifier<List<Produto>> {
+  final AppDatabase _db = AppDatabase();
+
+  ProdutoNotifier() : super([]) {
+    carregarProdutos();
+  }
+
+  Future<void> carregarProdutos() async {
+    final produtos = await _db.listarProdutos();
+    state = produtos;
+  }
+
+  Future<void> adicionarProduto(ProdutosCompanion produto) async {
+    await _db.inserirProduto(produto);
+    await carregarProdutos(); // Atualiza a lista
+  }
+
+  Future<void> excluirProduto(int id) async {
+    await _db.excluirProduto(id);
+    await carregarProdutos(); // Atualiza a lista
+  }
+}
