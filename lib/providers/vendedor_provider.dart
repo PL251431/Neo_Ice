4bb8b538a3461737d9e8f7f62e7515a7a00cf737
PL <pedrolucasdesousaoliveira@aluno.uespi.br>
2025-01-05 11:50:39 -0300

    feat: ajustando o provider dos vendedores

--- lib/providers/vendedor_provider.dart
+++ lib/providers/vendedor_provider.dart
@@ -1,7 +1,29 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:neo_ice/database/app_database.dart';
 
-final vendedorProvider = FutureProvider<List<Vendedor>>((ref) async {
-  final db = AppDatabase();
-  return await db.listarVendedores();
-});
+final vendedorProvider = StateNotifierProvider<VendedorNotifier, List<Vendedor>>(
+  (ref) => VendedorNotifier(),
+);
+
+class VendedorNotifier extends StateNotifier<List<Vendedor>> {
+  final AppDatabase _db = AppDatabase();
+
+  VendedorNotifier() : super([]) {
+    carregarVendedores();
+  }
+
+  Future<void> carregarVendedores() async {
+    final vendedores = await _db.listarVendedores();
+    state = vendedores;
+  }
+
+  Future<void> adicionarVendedor(VendedoresCompanion vendedor) async {
+    await _db.inserirVendedor(vendedor);
+    await carregarVendedores(); // Atualiza a lista
+  }
+
+  Future<void> excluirVendedor(int id) async {
+    await _db.excluirVendedor(id);
+    await carregarVendedores(); // Atualiza a lista
+  }
+}
