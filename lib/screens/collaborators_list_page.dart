9c09248c76eb4fc311a51b3ab7a58fbcbc094952
Kaio Simeão <k.rodrigues1704@gmail.com>
2025-01-06 20:29:58 -0300

    feat: adicinando as chamadas de modais na lista de colaboadores

--- lib/screens/collaborators_list_page.dart
+++ lib/screens/collaborators_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:neo_ice/database/app_database.dart';
+import 'package:neo_ice/widgets/collaborator_modal.dart';
 import 'package:neo_ice/widgets/new_collaborator_modal.dart';
 import 'package:neo_ice/providers/vendedor_provider.dart';
 import 'package:neo_ice/widgets/collaborator_card.dart';
@@ -11,28 +13,63 @@ class CollaboratorListPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final vendedores = ref.watch(vendedorProvider);
 
-    return Scaffold(
-      body: vendedores.isEmpty
-          ? const Center(child: Text('Nenhum vendedor encontrado.'))
-          : ListView.builder(
-              itemCount: vendedores.length,
-              itemBuilder: (context, index) {
-                final vendedor = vendedores[index];
-                return CollaboratorCard(
-                    nome: vendedor.nome, numero: vendedor.id);
-              },
-            ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AdicionarVendedorPage(),
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
+    return Stack(children: [
+      const Positioned(
+        top: 20,
+        left: 20,
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            'COLABORADORES',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
-    );
+      Positioned.fill(
+        top: 60,
+        child: vendedores.isEmpty
+            ? const Center(child: Text('Nenhum vendedor encontrado.'))
+            : ListView.builder(
+                itemCount: vendedores.length,
+                itemBuilder: (context, index) {
+                  final vendedor = vendedores[index];
+                  return GestureDetector(
+                    onTap: () {
+                      mostrarCollaboratorModal(
+                          context, AppDatabase(), vendedor.id);
+                    },
+                    child: CollaboratorCard(
+                      nome: vendedor.nome,
+                      numero: vendedor.id,
+                    ),
+                  );
+                },
+              ),
+      ),
+      Positioned(
+        bottom: 20,
+        right: 20,
+        child: FloatingActionButton(
+          onPressed: () {
+            mostrarNewCollaboratorModal(context, AppDatabase());
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    ]);
   }
+}
+
+mostrarNewCollaboratorModal(BuildContext context, AppDatabase db) {
+  showDialog(
+    context: context,
+    builder: (context) => NewCollaboratorModal(),
+  );
+}
+
+mostrarCollaboratorModal(BuildContext context, AppDatabase db, int id) {
+  showDialog(
+    context: context,
+    builder: (context) => CollaboratorModal(id: id),
+  );
 }
