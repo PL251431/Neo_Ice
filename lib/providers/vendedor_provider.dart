5c49a84f42e1f41751bdf090814488d774ac3992
Kaio Simeão <k.rodrigues1704@gmail.com>
2025-01-06 20:29:02 -0300

    feat: adicioando nova função ao vendedor provider

--- lib/providers/vendedor_provider.dart
+++ lib/providers/vendedor_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:neo_ice/database/app_database.dart';
 
-final vendedorProvider = StateNotifierProvider<VendedorNotifier, List<Vendedor>>(
+final vendedorProvider =
+    StateNotifierProvider<VendedorNotifier, List<Vendedor>>(
   (ref) => VendedorNotifier(),
 );
 
@@ -25,5 +26,13 @@ class VendedorNotifier extends StateNotifier<List<Vendedor>> {
   Future<void> excluirVendedor(int id) async {
     await _db.excluirVendedor(id);
     await carregarVendedores(); // Atualiza a lista
+  }
+
+  Vendedor? obterVendedorPorId(int id) {
+    try {
+      return state.firstWhere((vendedor) => vendedor.id == id);
+    } catch (e) {
+      return null;
+    }
   }
 }
