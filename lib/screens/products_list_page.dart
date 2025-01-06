3e8b4fabdd6ed10fa03f97af6aa4cc011d5390be
Kaio Simeão <k.rodrigues1704@gmail.com>
2025-01-06 19:45:33 -0300

    feat: adicionando a chamada do dialog de produto

--- lib/screens/products_list_page.dart
+++ lib/screens/products_list_page.dart
@@ -14,47 +14,58 @@ class ProdutcsListPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final produtos = ref.watch(produtoProvider);
 
-    return Stack(children: [
-      produtos.isEmpty
-          ? const Center(child: Text('Nenhum produto encontrado.'))
-          : ListView.builder(
-              itemCount: produtos.length,
-              itemBuilder: (context, index) {
-                final produto = produtos[index];
-                final imageProvider = File(produto.imagem).existsSync()
-                    ? FileImage(File(produto.imagem))
-                    : const AssetImage('assets/images/default.png')
-                        as ImageProvider<Object>;
+    return Stack(
+      children: [
+        const Positioned(
+          top: 20,
+          left: 20,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'PRODUTOS',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          top: 60,
+          child: produtos.isEmpty
+              ? const Center(child: Text('Nenhum produto encontrado.'))
+              : ListView.builder(
+                  itemCount: produtos.length,
+                  itemBuilder: (context, index) {
+                    final produto = produtos[index];
+                    final imageProvider = File(produto.imagem).existsSync()
+                        ? FileImage(File(produto.imagem))
+                        : const AssetImage('assets/images/default.png')
+                            as ImageProvider<Object>;
 
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProdutoPage(), // Ajustar aqui
+                    return GestureDetector(
+                      onTap: () {
+                        mostrarProdutoModal(context, AppDatabase(), produto.id);
+                      },
+                      child: ProductCard(
+                        image: imageProvider,
+                        name: produto.nome,
+                        valor: produto.valor,
+                        context: context,
                       ),
                     );
                   },
-                  child: ProductCard(
-                    image: imageProvider,
-                    name: produto.nome,
-                    valor: produto.valor,
-                    context: context,
-                  ),
-                );
-              },
-            ),
-      Positioned(
-        bottom: 20,
-        right: 20,
-        child: FloatingActionButton(
-          onPressed: () {
-            mostrarAdicionarProdutoModal(context, AppDatabase());
-          },
-          child: const Icon(Icons.add),
+                ),
         ),
-      ),
-    ]);
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: FloatingActionButton(
+            onPressed: () {
+              mostrarAdicionarProdutoModal(context, AppDatabase());
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ],
+    );
   }
 }
 
@@ -62,5 +73,12 @@ mostrarAdicionarProdutoModal(BuildContext context, AppDatabase db) {
   showDialog(
     context: context,
     builder: (context) => AdicionarProdutoModal(db: db),
+  );
+}
+
+mostrarProdutoModal(BuildContext context, AppDatabase db, int id) {
+  showDialog(
+    context: context,
+    builder: (context) => ProdutcModal(id: id),
   );
 }
